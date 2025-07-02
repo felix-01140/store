aee825803cfe9846c50019a16ff1116888c28df2
felix-01140 <urumeneshaa@gmail.com>
2025-07-02 09:46:44 +0200

    add numbr

--- products.js
+++ products.js
@@ -13,9 +13,9 @@ window.PRODUCTS = [
   { id: 10, name: "Hoodie", price: 13000, image: "products/Hoodie.jpg", images: ["products/Hoodie.jpg"], category: "Clothing" },
   { id: 11, name: "Indoors Open", price: 6500, image: "products/Indoors Open.jpg", images: ["products/Indoors Open.jpg"], category: "Shoes" },
   { id: 14, name: "Kids open shoes", price: 10000, image: "products/Kids open hoes.jpg", images: ["products/Kids open hoes.jpg"], category: "Shoes" },
-  { id: 15, name: "kids Open", price: 10000, image: "products/kids Open.jpg", images: ["products/kids Open.jpg"], category: "Shoes" },
-  { id: 16, name: "Large hats", price: 36000, image: "products/Large hats .jpg", images: ["products/Large hats .jpg"], category: "Clothing" },
-  { id: 17, name: "long jacket", price: 30000, image: "products/long jacket.jpg", images: ["products/long jacket.jpg"], category: "Clothing" },
+  { id: 15, name: "kids Open", price: 3800, image: "products/kids Open.jpg", images: ["products/kids Open.jpg"], category: "Shoes" },
+  { id: 16, name: "Large hats", price: 9000, image: "products/Large hats .jpg", images: ["products/Large hats .jpg"], category: "Clothing" },
+  { id: 17, name: "long jacket", price: 36000, image: "products/long jacket.jpg", images: ["products/long jacket.jpg"], category: "Clothing" },
   { id: 18, name: "long sleeve shirt", price: 12000, image: "products/long sleeve shirt.jpg", images: ["products/long sleeve shirt.jpg"], category: "Clothing" },
   { id: 19, name: "men hats", price: 6500, image: "products/men hats.jpg", images: ["products/men hats.jpg"], category: "Clothing" },
   { id: 21, name: "miami shorts", price: 7000, image: "products/miami shorts.jpg", images: ["products/miami shorts.jpg"], category: "Clothing" },
@@ -27,6 +27,7 @@ window.PRODUCTS = [
   { id: 27, name: "cotton sweater", price: 15500, image: "products/cottonsweater.jpg", images: ["products/cottonsweater.jpg"], category: "Clothing" },
   { id: 28, name: "Men shoes", price: 17000, image: "products/menshoes.jpg", images: ["products/menshoes.jpg"], category: "Shoes" },
   { id: 29, name: "Sports Shoes for men & women", price: 25000, image: "products/shoesformen.jpg", images: ["products/shoesformen.jpg"], category: "Shoes" },
-  { id: 30, name: "Sports shoes", price: 19000, image: "products/sportsshoes.jpg", images: ["products/sportsshoes.jpg"], category: "Shoes" }
+  { id: 30, name: "Sports shoes", price: 19000, image: "products/sportsshoes.jpg", images: ["products/sportsshoes.jpg"], category: "Shoes" },
+  { id: 31, name: "Boots ", price: 8000, image: "products/boots.jpg", images: ["products/boots.jpg"], category: "Shoes" }
   
 ];
